23231366da14272ac55477220d8344ab50a75511
fsfuseini <suhuyini86@gmail.com>
2025-07-10 16:48:59 +0000

    Updated login

--- src/validators/users.js
+++ src/validators/users.js
@@ -12,6 +12,7 @@ export const resgisterUserValidator = Joi.object({
 export const loginUserValidator = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
+    role: Joi.string().valid("user", "admin").required()
 });
 
 // Update Password
