455955144728270474630f51ea294c1da7691424
fsfuseini <suhuyini86@gmail.com>
2025-07-14 17:05:39 +0000

    more on users

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,7 +9,7 @@ userRouter.post("/users/register", registerUser);
 userRouter.post("/users/login", loginUser);
 userRouter.post("/users/update-password", isAuthenticated, updatePassword);
 userRouter.post("/users/reset-password", isAuthenticated, resetPassword);
-userRouter.get("/admin/users", isAuthenticated, getAllUsers);
+userRouter.get("/admin/users", isAuthenticated, isAdmin, getAllUsers);
 userRouter.get("/admin/users/:id", isAuthenticated, getSingleUser);
 userRouter.delete("/admin/users/:id", isAuthenticated, isAdmin, deleteUser);
 
